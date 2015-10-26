c28e021bd670382699af6b7849a6f4cb88318af9
owil <lacheinne@gmail.com>
2015-10-26 12:57:47 -0400

    Modified Manifest and Main Activity xml+java
    Also added a string name in strings xml and added a drawable,.

--- AndroidService/app/src/main/java/io/github/owil/androidservice/MainActivity.java
+++ AndroidService/app/src/main/java/io/github/owil/androidservice/MainActivity.java
@@ -1,6 +1,5 @@
 package io.github.owil.androidservice;
 
-import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
