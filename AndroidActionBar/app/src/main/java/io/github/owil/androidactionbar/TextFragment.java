28282190c84dc026aca9690948a84087b1275af1
owil <lacheinne@gmail.com>
2015-10-26 10:34:36 -0400

    Modified Main and Text Fragment java
    Modified the onCreateView methods and removed unused methods and
    variables.

--- AndroidActionBar/app/src/main/java/io/github/owil/androidactionbar/TextFragment.java
+++ AndroidActionBar/app/src/main/java/io/github/owil/androidactionbar/TextFragment.java
@@ -1,30 +1,25 @@
 package io.github.owil.androidactionbar;
 
-
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class TextFragment extends Fragment {
-
-
+    TextView text;
     public TextFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text, container, false);
+        View view = inflater.inflate(R.layout.fragment_text,container,false);
+        text = (TextView)view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
     }
-
-
 }
